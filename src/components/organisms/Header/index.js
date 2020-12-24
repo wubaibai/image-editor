f6363b02abcc130c1e47491c58cd281e2193790b
Cathy Retina 13 <a304126@gmail.com>
2020-12-24 14:04:40 +0800

    feat: import image

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
+import Button from 'components/atoms/Button';
 import style from './index.css';
 
-const Header = () => (
-	<div className={style.header}>
-		<div>Image Editor</div>
-		<div>Buttons</div>
-	</div>
-);
+const Header = () => {
+	const fileInputRef = useRef();
+
+	const readFile = input => {
+		if (input.files && input.files[0]) {
+			const reader = new FileReader();
+			reader.onload = e => {
+				const img = new Image();
+				img.src = e.target.result;
+				img.onload = () => {
+					console.log(img);
+				};
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
+	};
+
+	const onImportClick = () => {
+		fileInputRef.current.click();
+	};
+
+	const onFileInputChanged = e => {
+		readFile(e.target);
+	};
+
+	return (
+		<div className={style.header}>
+			<div>Image Editor</div>
+			<div className={style.actions}>
+				<Button onClick={onImportClick}>Import</Button>
+				<input type="file" hidden ref={fileInputRef} onChange={onFileInputChanged} />
+				<Button>Export</Button>
+			</div>
+		</div>
+	);
+};
 
 export default Header;
