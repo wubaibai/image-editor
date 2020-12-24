a911cba160691e831de1e89940b7b7589bc0819a
Cathy Retina 13 <a304126@gmail.com>
2020-12-24 15:16:17 +0800

    feat: get image data from redux and add to editor

--- src/components/organisms/Editor/index.js
+++ src/components/organisms/Editor/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { connect } from 'react-redux';
 
 import style from './index.css';
 
-const Editor = () => {
-	return <div className={style.editor}>Editor</div>;
+const Editor = ({ image }) => {
+	const editorRef = useRef();
+
+	useEffect(() => {
+		console.log('image changed');
+		console.log(image);
+
+		if (editorRef.current && image.data) {
+			editorRef.current.appendChild(image.data);
+		}
+	}, [image]);
+
+	return (
+		<div className={style.editor} ref={editorRef}>
+			Editor
+		</div>
+	);
 };
 
-export default Editor;
+const mapStateToProps = state => ({
+	image: state.image,
+});
+
+export default connect(mapStateToProps, {})(Editor);
