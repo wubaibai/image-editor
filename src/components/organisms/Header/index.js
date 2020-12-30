32fb524005360a5a70798cd654c5eea8936f179d
Cathy Retina 13 <a304126@gmail.com>
2020-12-30 14:23:53 +0800

    refactor: move export confirm into modal action button area

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -1,16 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { saveAs } from 'file-saver';
 
 import { addImage } from 'actions/image';
 import Button from 'components/atoms/Button';
+import Modal from 'components/molecules/Modal';
 import style from './index.css';
 import useActions from '../../../utils/useAction';
 
 const Header = () => {
 	const fileInputRef = useRef();
+	const [isExportModal, setIsExportModal] = useState(false);
 	const image = useSelector(state => state.image);
 	const addImageAction = useActions(addImage);
+	const [mimeType, setMimeType] = useState('image/jpeg');
+	const [quality, setQuality] = useState(1);
 
 	const readFile = input => {
 		if (input.files && input.files[0]) {
@@ -54,7 +58,7 @@ const Header = () => {
 		});
 	};
 
-	const onExportClick = async () => {
+	const onExportConfirm = async () => {
 		const exportCanvas = document.createElement('CANVAS');
 		const exportCtx = exportCanvas.getContext('2d');
 		exportCanvas.width = image.width;
@@ -69,16 +73,39 @@ const Header = () => {
 				blob => {
 					saveAs(blob, 'exported');
 				},
-				'image/jpeg',
-				0.95,
+				mimeType,
+				quality,
 			);
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
+	const onExportClick = async () => {
+		setIsExportModal(true);
+	};
+
 	const onFileInputChanged = e => {
 		readFile(e.target);
+	};
+
+	const renderExportModal = () => {
+		const content = (
+			<div>
+				MIME Type: {mimeType}, Quality: {quality}
+			</div>
+		);
+		const actions = <Button onClick={onExportConfirm}>Export</Button>;
+
+		return (
+			<Modal
+				title="匯出圖片"
+				content={content}
+				actions={actions}
+				visible={isExportModal}
+				onDismiss={() => setIsExportModal(false)}
+			/>
+		);
 	};
 
 	return (
@@ -88,6 +115,7 @@ const Header = () => {
 				<Button onClick={onImportClick}>Import</Button>
 				<input type="file" hidden ref={fileInputRef} onChange={onFileInputChanged} />
 				<Button onClick={onExportClick}>Export</Button>
+				{renderExportModal()}
 			</div>
 		</div>
 	);
